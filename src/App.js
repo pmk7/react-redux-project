ca0bbccc91fc8a8aa9c5ce9f994be9e264b4790d
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-11-16 17:17:16 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React from 'react';
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 // items
-import cartItems from "./cart-items";
+import cartItems from './cart-items';
 // redux stuff
+import { createStore } from 'redux';
+
+import reducer from './reducer';
+
+import { Provider } from 'react-redux';
+
+// initial store
+const initalStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
+// store
+const store = createStore(reducer, initalStore);
 
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
       <CartContainer cart={cartItems} />
-    </main>
+    </Provider>
   );
 }
 
