639a5580f489baf34398ad858c4428a662d4ad05
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-12-08 16:12:16 +0100

    third commit

--- src/reducer.js
+++ src/reducer.js
@@ -5,13 +5,36 @@ const reducer = (state, action) => {
     return { ...state, cart: [] };
   }
   if (action.type === DECREASE) {
-    console.log('you decreased amount');
+    let tempCart = [];
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      );
+    } else {
+      tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+      return { ...state, cart: tempCart };
+    }
+    return { ...state, cart: tempCart };
   }
   if (action.type === INCREASE) {
-    console.log('you increased amount');
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
   }
   if (action.type === REMOVE) {
-    console.log('you removed amount');
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload.id),
+    };
   }
   return state;
 };
